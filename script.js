a14418e2a230efaeb6ec3487ac8ffc39100d76e0
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-05-03 01:30:58 -0500

    worked on navbar for wider display

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ $(document).ready(function () {
     let isOpen = $("#nav-links").hasClass("open");
     $("#menu-btn i").attr("class", isOpen ? "ri-close-line" : "ri-menu-line");
   });
+
+    $("#nav-links a").on("click", function (e) {
+        $("#nav-links").removeClass("open");
+        $("#menu-btn i").attr("class", "ri-menu-line");
+    });
 });
