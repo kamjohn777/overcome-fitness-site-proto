ed95e5204192df5d767b36b54c8eee9e6f48f97c
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-06-12 20:11:05 -0500

    worked on the api call for videos from youtube

--- exerciseScript.js
+++ exerciseScript.js
@@ -1,35 +1,33 @@
-// Get the input values from localStorage
-const fitnessGoals = localStorage.getItem('fitnessGoals');
-const dietGoals = localStorage.getItem('dietGoals');
-const mealType = localStorage.getItem('mealType');
-const caloriesRange = localStorage.getItem('caloriesRange');
-
-// Fetch the exercises from the ExerciseDB API
-$.get('https://wger.de/api/v2/exercise/?language=2&limit=10&muscles=' + fitnessGoals, function(data) {
-  // Clear the exercises div
-  $('#exercises').empty();
-
-  // Loop through the exercises in the response
-  for (let exercise of data.results) {
-    // Create a new div for the exercise
-    let exerciseDiv = $('<div>').addClass('exercise');
-
-    // Add the exercise name and description to the div
-    exerciseDiv.append($('<h2>').text(exercise.name));
-    exerciseDiv.append($('<p>').html(exercise.description));
-
-    // Add a video and play button if the exercise has a video
-    if (exercise.video_url) {
-      let video = $('<video>').attr('src', exercise.video_url).attr('id', 'video' + exercise.id);
-      let button = $('<button>').text('Play').on('click', function() {
-        video.get(0).play();
+$(document).ready(function() {
+    $('#filter-form').on('submit', function(e) {
+      e.preventDefault();
+  
+      var workoutType = $('#workout-type').val();
+      var bodyPart = $('#body-part').val();
+      var difficultyLevel = $('#difficulty-level').val();
+      var duration = $('#duration').val();
+      var equipmentNeeded = $('#equipment-needed').val();
+  
+      var query = workoutType + ' ' + bodyPart + ' ' + difficultyLevel + ' ' + duration + ' ' + equipmentNeeded;
+  
+      console.log('Form submitted with query:', query);
+  
+      $.get('http://localhost:3000/youtube', {
+        q: query
+      }, function(data) {
+        console.log('Received data from YouTube API:', data);
+  
+        $('#exercises').empty();
+  
+        $.each(data.items, function(i, item) {
+          var videoId = item.id.videoId;
+          var title = item.snippet.title;
+          var description = item.snippet.description;
+  
+          $('#exercises').append('<h3>' + title + '</h3><p>' + description + '</p><iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        });
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error('Error making YouTube API request:', textStatus, errorThrown);
       });
-
-      exerciseDiv.append(video);
-      exerciseDiv.append(button);
-    }
-
-    // Append the exercise div to the exercises div
-    $('#exercises').append(exerciseDiv);
-  }
-});
+    });
+  });
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors()); // Use cors middleware
+const port = 3000;
+
+app.get('/youtube', async (req, res) => {
+  try {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        q: req.query.q,
+        type: 'video',
+        key: process.env.YOUTUBE_API_KEY
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error making YouTube API request:', error);
+    res.status(500).send('Error making YouTube API request');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
